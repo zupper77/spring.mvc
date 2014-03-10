a7c2ca0b87f24d776dde91d0cc17f46f4204384e
yck <ck.you@wemakeprice.co.kr>
2014-03-10 10:44:00 +0900

    test2

---
+++ src/main/java/com/wemakeprice/vo/info/Info.java
@@ -0,0 +1,31 @@
+package com.wemakeprice.vo.info;
+
+public class Info {
+	
+	private int id;
+	private String title;
+	private String writer;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	
+	
+	
+	
+	
+}
