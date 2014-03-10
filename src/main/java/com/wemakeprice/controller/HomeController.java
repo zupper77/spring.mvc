b3b83db27736f5c2fb96bdef0b83aabd26dca226
yck <ck.you@wemakeprice.co.kr>
2014-03-10 10:42:50 +0900

    test

--- src/main/java/com/wemakeprice/controller/HomeController.java
+++ src/main/java/com/wemakeprice/controller/HomeController.java
@@ -1,15 +1,27 @@
 package com.wemakeprice.controller;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wemakeprice.vo.board.BoardVO;
+import com.wemakeprice.vo.info.Info;
 
 /**
  * Handles requests for the application home page.
@@ -22,6 +34,13 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	
+	@ModelAttribute("searchTeamList")
+	public String[] getSearchTeamList(){
+		return new String[]{"개발1팀","개발2팀","개발3팀"};
+	}
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -33,7 +52,87 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		
+		
+		
+		return "index";
 	}
+	 
+	
+	 @RequestMapping(value="/mappingJacksonJsonView", method = {RequestMethod.GET,RequestMethod.POST})
+	 public List<BoardVO> mappingJacksonJsonView_Example(HttpServletRequest request, HttpServletResponse response , Model model){
+	  System.out.println("----- mappingJacksonJsonView_Example -----");
+	  
+	  List<BoardVO> list = new ArrayList<BoardVO>();
+	  ModelAndView modelAndView = new ModelAndView();
+	  
+	  
+	  BoardVO b_vo1 = new BoardVO();
+	  b_vo1.setId(1);
+	  b_vo1.setTitle("test1");
+	  b_vo1.setWriter("testtest1");
+	  BoardVO b_vo2 = new BoardVO();
+	  b_vo2.setId(2);
+	  b_vo2.setTitle("test2");
+	  b_vo2.setWriter("testtest2");
+	  BoardVO b_vo3 = new BoardVO();
+	  b_vo3.setId(3);
+	  b_vo3.setTitle("test3");
+	  b_vo3.setWriter("testtest3");
+	  
+	  list.add(b_vo1);
+	  list.add(b_vo2);
+	  list.add(b_vo3);
+	  
+	  
+	  
+	  //modelAndView.addObject("dataList", list);
+	  
+	  
+	  
+	  return list;
+	 }
+	 
+	 
+	 @RequestMapping(value="/castorMarshaller", method = {RequestMethod.GET,RequestMethod.POST})
+	 public ModelAndView castorMarshaller_Example(HttpServletRequest request, HttpServletResponse response , Model model){
+	  System.out.println("----- castorMarshallerXmlView_Example -----");
+	  
+	  List<Info> list = new ArrayList<Info>();
+	  
+	  Info vo1 = new Info();
+	  Info vo2 = new Info();
+	  Info vo3 = new Info();
+	  
+	  vo1.setId(1);
+	  vo1.setTitle("test1");
+	  vo1.setWriter("value1");
+	  
+	  vo1.setId(2);
+	  vo2.setTitle("test2");
+	  vo2.setWriter("value2");
+	  
+	  vo3.setId(3);
+	  vo3.setTitle("test3");
+	  vo3.setWriter("value3");
+	  
+	  
+	  list.add(vo1);
+	  list.add(vo2);
+	  list.add(vo3);
+	  
+	  Map<String,Object> map = new HashMap();
+	  map.put("*-info", list);
+
+	  //new ModelAndView(viewName, modelName, modelObject)
+	  
+	  return new ModelAndView("", map);	  
+	  //return new ModelAndView(map);
+	 }
+	 
+	
+	
+	
+	
 	
 }
--- src/test/java/com/wemakeprice/common/test.java
+++ src/test/java/com/wemakeprice/common/test.java
@@ -1,5 +1,36 @@
 package com.wemakeprice.common;
 
-public class test {
+import java.util.Enumeration;
+import java.util.Vector;
 
+public class test {
+	
+	public static void main(String[]args){
+		
+		Vector<String> vc = new Vector<String>();
+		
+	    vc.add("Vector Object 1");
+	    vc.add("Vector Object 2");
+	    vc.add("Vector Object 3");
+	    vc.add("Vector Object 4");
+	    vc.add("Vector Object 5");
+		
+	    Enumeration<String> enu = vc.elements();
+	    
+	    while(enu.hasMoreElements()){
+	    	String data = enu.nextElement();
+	    	System.out.println(data);
+	    	
+	    }
+	    
+	    
+	    
+	    
+	}
+	
+
+	
+    
+	
+	
 }
